4b75f23af9fbe2636fb7f8f1e833a58944307f21
sampo1 <33290683+sampo1@users.noreply.github.com>
2021-01-19 12:19:11 +0200

    UDP Chatpalvelin ja -asiakas

---
+++ ChatAsiakasUDP/KaikuAsiakas/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+/// <summary>
+/// UDP-Chatin asiakassovellus
+/// </summary>
+namespace Chatasiakas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket soketti = null;
+            Console.WriteLine("Chatasiakassovellus UDP");
+
+            soketti = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            int port = 9999;
+
+            System.Net.IPEndPoint iep = new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, port); // IPEndPoint luokan olio, alustettu arvoon null
+
+            //IPEndPoint sender = new IPEndPoint(IPAddress.Loopback, 0); // Vastaanotetun paketin lähettäjä
+            EndPoint senderRemote = (EndPoint)iep; // Typecast EndPoint:ksi
+            soketti.ReceiveTimeout = 1000; // Lopetetaan jos palvelin ei lähetä mitään 3000 ms
+            byte[] rec = new byte[2048]; // Tavutaulukko vastaanottoa varten
+                                        
+            String viesti = "";
+            Boolean jatketaanko = true;
+            do
+            {
+                Console.Write(">");
+                viesti = Console.ReadLine();
+                if (viesti.Equals("sulje"))
+                {
+                    jatketaanko = false;
+                }
+                else
+                {
+                    soketti.SendTo(Encoding.ASCII.GetBytes(viesti), senderRemote);
+
+                    while (!Console.KeyAvailable)
+                    {
+                        IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+                        EndPoint PalvelinEP = (EndPoint)remote;
+
+                        try
+                        {
+                            int received = soketti.ReceiveFrom(rec, ref PalvelinEP);
+                            String vastString = Encoding.ASCII.GetString(rec, 0, received);
+                            char[] delim = { ';' };
+                            String[] palat = vastString.Split(delim, 2);
+                            if (palat.Length < 2)
+                            {
+                                Console.WriteLine("Virheellisen muotoinen viesti, anna viesti muotoa (lähettäjä;viesti)");
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0}: {1}", palat[0], palat[1]);
+                            }
+                        }
+                        catch
+                        {
+                            // timeout
+                        }
+                    }
+                }
+            } while (jatketaanko);
+
+            soketti.Close();
+
+        }
+    }
+}
---
+++ ChatpalvelinUDP/Kaikupalvelin/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// UDP Chatpalvelinsovellus
+/// </summary>
+namespace Kaikupalvelin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket palvelin = null;
+            IPEndPoint iep = new IPEndPoint(IPAddress.Loopback, 9999);
+
+            List<EndPoint> asiakkaat = new List<EndPoint>();
+            Console.WriteLine("Chatpalvelinsovellus");
+            try
+            {
+                palvelin = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                palvelin.Bind(iep);
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine("Virhe... " + ex.Message);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("odotetaan asiakasta...");
+
+            while (!Console.KeyAvailable)
+            {
+                byte[] rec = new byte[256];
+                IPEndPoint asiakas = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint remote = (EndPoint)asiakas;
+                int received = palvelin.ReceiveFrom(rec, ref remote);
+
+                String vastString = Encoding.ASCII.GetString(rec, 0, received);
+                char[] delim = { ';' };
+                String[] palat = vastString.Split(delim, 2);
+                if (palat.Length < 2)
+                {
+                    Console.WriteLine("Virheellisen muotoinen viesti, anna viesti muotoa (lähettäjä;viesti)");
+                }
+                else
+                {
+                    if (!asiakkaat.Contains(remote))
+                    {
+                        asiakkaat.Add(remote);
+                        Console.WriteLine("Uusi asiakas: [{0}:{1}]", ((IPEndPoint)remote).Address, ((IPEndPoint)remote).Port);
+                    }
+                    Console.WriteLine("{0}: {1}", palat[0], palat[1]);
+                    foreach(EndPoint client in asiakkaat)
+                    {
+                        palvelin.SendTo(Encoding.ASCII.GetBytes(vastString), client);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+            palvelin.Close();
+            
+        }
+    }
+}
+
